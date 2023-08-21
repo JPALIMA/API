0c52d99b8b3422e356685956874c3bc81a21d4da
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-08-21 20:57:42 -0300

    Configuração de Rotas as rotas para as ações definidas no controlador

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
-end
+  def index
+    posts = Post.all
+    render json: posts
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post, status: :created
+    else
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
+
+  #Outras ações (update, delete) podem ser adicionadas aqui
+
+  private
+
+  def post_params
